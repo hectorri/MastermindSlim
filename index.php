10db6528989d98745cae16dc52cf730c010e7c46
Héctor <hectorri@hotmail.es>
2018-04-26 19:39:12 +0200

    Subida inicial de esqueleto Slim

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require("vendor/autoload.php");
+ 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+ 
+require 'vendor/autoload.php';
+ 
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response) {
+	echo "Página  principal de MasterMindSlim";
+});
+$app->run();
+?>
