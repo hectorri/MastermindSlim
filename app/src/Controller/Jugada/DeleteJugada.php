5599fcda445fcadbaa0c23cbeb9d2b570a598771
Raquel R <raquelrh@gmail.com>
2018-04-29 22:44:05 +0200

    Borrado de código innecesario

--- app/src/Controller/Jugada/DeleteJugada.php
+++ app/src/Controller/Jugada/DeleteJugada.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Jugada;
 
-use App\Controller\BaseController;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Container;
@@ -12,21 +11,7 @@ use Slim\Container;
  */
 class DeleteJugada extends BaseJugada {
 
-	protected $jugadaResource;
-
-	public function __construct(Container $container)
-	{
-	  $this->logger = $container->get('logger');
-	  $this->jugadaResource = $container->get('jugada_resource');
-	}
-  
-	protected function getJugadaResource()
-	{
-	  return $this->jugadaResource;
-	}
-
-    public function __invoke($request, $response, $args){
-
+  public function __invoke($request, $response, $args){
 	    $this->setParams($request, $response, $args);
       //$input = $this->getInput();
       $idJugada = $this->args['idJugada'];
