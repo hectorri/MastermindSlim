d2a10f43aea090d582d09994f12088ef155c71ff
Raquel R <raquelrh@gmail.com>
2018-04-29 18:17:51 +0200

    Modificaciones en delete jugada

--- app/src/Controller/Jugada/DeleteJugada.php
+++ app/src/Controller/Jugada/DeleteJugada.php
@@ -10,23 +10,26 @@ use Slim\Container;
 /**
  * Delete Jugada Controller.
  */
-class DeleteJugada extends BaseControler {
+class DeleteJugada extends BaseController {
 
-	protected $partidaResource;
+	protected $jugadaResource;
 
-	public function __construct(Container $container){
-		$this->logger = $container->get('logger');
-		$this->partidaResource = $container->get('partida_resource');
+	public function __construct(Container $container)
+	{
+	  $this->logger = $container->get('logger');
+	  $this->jugadaResource = $container->get('jugada_resource');
+	}
+  
+	protected function getJugadaResource()
+	{
+	  return $this->jugadaResource;
 	}
 
-	protected function getPartidaResource() {
-		return $this->partidaResource;
-	}
     public function __invoke($request, $response, $args){
 
         $this->setParams($request, $response, $args);
         $nombre = $this->args['nombre'];
-        $result = $this->getJugadaResource()->deletePartida($nombre);
+        $result = $this->getJugadaResource()->deleteJugada($nombre);
 
         return $this->jsonResponse('success', $result, 200);
     }
