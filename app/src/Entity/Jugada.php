30275a7f777b330335778e90d6538c75c5763d80
Héctor <hectorri@hotmail.es>
2018-04-30 19:27:30 +0200

    Comentarios

--- app/src/Entity/Jugada.php
+++ app/src/Entity/Jugada.php
@@ -5,14 +5,14 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Jugada
+ * Representa la clase Jugada
  *
  * @ORM\Table(name="jugada")
  * @ORM\Entity
  */
-class Jugada
-{
+class Jugada {
     /**
+	 * Identificador de la jugada
      * @var int
      *
      * @ORM\Column(name="id_jugada", type="integer", nullable=false)
@@ -22,6 +22,7 @@ class Jugada
     private $idJugada;
 
     /**
+	 * Nombre de la partida a la que pertenece la jugada
      * @var string
      *
      * @ORM\Column(name="nombre_partida", type="string", length=100, nullable=false)
@@ -31,6 +32,7 @@ class Jugada
     private $nombrePartida;
 
     /**
+	 * Fecha de ejecución de la jugada
      * @var \DateTime
      *
      * @ORM\Column(name="fecha", type="datetime", nullable=false)
@@ -38,6 +40,7 @@ class Jugada
     private $fecha;
 
     /**
+	 * Código de colores de la jugada
      * @var string
      *
      * @ORM\Column(name="codigo_jugada", type="string", length=6, nullable=false)
@@ -45,6 +48,7 @@ class Jugada
     private $codigoJugada;
 
     /**
+	 * Resultado de cotejar la jugada con la combinación
      * @var string
      *
      * @ORM\Column(name="resultado_jugada", type="string", length=6, nullable=false)
@@ -54,122 +58,107 @@ class Jugada
 
 
     /**
-     * Set idJugada.
+     * Establece el identificador de la jugada
      *
      * @param int $idJugada
      *
      * @return Jugada
      */
-    public function setIdJugada($idJugada)
-    {
+    public function setIdJugada($idJugada) {
         $this->idJugada = $idJugada;
-
         return $this;
     }
 
     /**
-     * Get idJugada.
+     * Obtiene el identificador de la jugada
      *
      * @return int
      */
-    public function getIdJugada()
-    {
+    public function getIdJugada() {
         return $this->idJugada;
     }
 
     /**
-     * Set nombrePartida.
+     * Establece el nombre de la partida
      *
      * @param string $nombrePartida
      *
      * @return Jugada
      */
-    public function setNombrePartida($nombrePartida)
-    {
+    public function setNombrePartida($nombrePartida) {
         $this->nombrePartida = $nombrePartida;
-
         return $this;
     }
 
     /**
-     * Get nombrePartida.
+     * Obtiene el nombre de la partida
      *
      * @return string
      */
-    public function getNombrePartida()
-    {
+    public function getNombrePartida() {
         return $this->nombrePartida;
     }
 
     /**
-     * Set fecha.
+     * Establece la fecha de ejecución de la jugada
      *
      * @param \DateTime $fecha
      *
      * @return Jugada
      */
-    public function setFecha($fecha)
-    {
+    public function setFecha($fecha) {
         $this->fecha = $fecha;
-
         return $this;
     }
 
     /**
-     * Get fecha.
+     * Obtiene la fecha de ejecución de la jugada
      *
      * @return \DateTime
      */
-    public function getFecha()
-    {
+    public function getFecha() {
         return $this->fecha;
     }
 
     /**
-     * Set codigoJugada.
+     * Establece el código de colores de la jugada
      *
      * @param string $codigoJugada
      *
      * @return Jugada
      */
-    public function setCodigoJugada($codigoJugada)
-    {
+    public function setCodigoJugada($codigoJugada) {
         $this->codigoJugada = $codigoJugada;
-
         return $this;
     }
 
     /**
-     * Get codigoJugada.
+     * Obtiene el código de colores de la jugada.
      *
      * @return string
      */
-    public function getCodigoJugada()
-    {
+    public function getCodigoJugada() {
         return $this->codigoJugada;
     }
 
     /**
-     * Set resultadoJugada.
+     * Establece el resultado de la jugada
      *
      * @param string $resultadoJugada
      *
      * @return Jugada
      */
-    public function setResultadoJugada($resultadoJugada)
-    {
+    public function setResultadoJugada($resultadoJugada) {
         $this->resultadoJugada = $resultadoJugada;
-
         return $this;
     }
 
     /**
-     * Get resultadoJugada.
+     * Obtiene el resultado de la jugada
      *
      * @return string
      */
-    public function getResultadoJugada()
-    {
+    public function getResultadoJugada() {
         return $this->resultadoJugada;
     }
 }
