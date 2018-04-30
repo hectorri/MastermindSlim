6eb8574c56d484da6d82dc14ad31237b4bef0473
Héctor <hectorri@hotmail.es>
2018-04-30 19:48:51 +0200

    Comentarios

--- app/src/Resource/BaseResource.php
+++ app/src/Resource/BaseResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Resource;
 
 use Slim\Http\Request;
@@ -9,51 +8,50 @@ use \Doctrine\ORM\Tools\Setup;
 use \Doctrine\ORM\EntityManager;
 
 /**
- * Base Resource.
+ * Representa la parte común de los gestores de datos
  */
-abstract class BaseResource
-{
-    /**
-     * @var Logger $logger
-     */
-    protected $logger;
+abstract class BaseResource {
+	/**
+	 * @var Logger $logger
+	 */
+	protected $logger;
 
-  /**
-   * @var \Doctrine\ORM\EntityManager
-   */
-  private $entityManager = null;
+	/**
+	 * Referencia al gestor de datos
+	 * @var \Doctrine\ORM\EntityManager
+	 */
+	private $entityManager = null;
 
-  /**
-   * @return \Doctrine\ORM\EntityManager
-   */
-  public function getEntityManager()
-  {
-    if ($this->entityManager === null) {
-      $this->entityManager = $this->createEntityManager();
-    }
+	/**
+	 * Obtiene el gestor de datos
+	 * @return \Doctrine\ORM\EntityManager
+	 */
+	public function getEntityManager() {
+		if ($this->entityManager === null) {
+			$this->entityManager = $this->createEntityManager();
+		}
+		return $this->entityManager;
+	}
 
-    return $this->entityManager;
-  }
+	/**
+	 * Representa el gestor de datos e incluye los parámetros de conexión a la BD
+	 * @return EntityManager
+	 */
+	public function createEntityManager() {
+		$path = array('app/src/Entity');
+		$devMode = true;
 
-  /**
-   * @return EntityManager
-   */
-  public function createEntityManager()
-  {
-    $path = array('app/src/Entity');
-    $devMode = true;
+		$config = Setup::createAnnotationMetadataConfiguration($path, $devMode,
+		null, null, false);
 
-    $config = Setup::createAnnotationMetadataConfiguration($path, $devMode,
-      null, null, false);
+		$connectionOptions = array(
+		'driver'   => 'pdo_mysql',
+		'host'	 => 'localhost',
+		'dbname'   => 'mastermindslim',
+		'user'	 => 'mastermindslim',
+		'password' => 'mastermindslim',
+		);
 
-    $connectionOptions = array(
-      'driver'   => 'pdo_mysql',
-      'host'	 => 'localhost',
-      'dbname'   => 'mastermindslim',
-      'user'	 => 'mastermindslim',
-      'password' => 'mastermindslim',
-    );
-
-    return EntityManager::create($connectionOptions, $config);
-  }
+		return EntityManager::create($connectionOptions, $config);
+	}
 }
--- app/src/Resource/Partida/PartidaResource.php
+++ app/src/Resource/Partida/PartidaResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Resource\Partida;
 
 use \DateTime;
@@ -7,95 +6,116 @@ use App\Entity\Partida;
 use App\Resource\BaseResource;
 use Slim\Container;
 
-class PartidaResource extends BaseResource
-{
-  /**
-   * @param Container $container
-   */
-  public function __construct(Container $container)
-  {
-    $this->logger = $container->get('logger');
-  }
+/**
+ * Representa el gestor de Partidas
+ */
+class PartidaResource extends BaseResource {
 
-  public function getPartidas()
-  {
-    $partidas = $this->getEntityManager()->getRepository('App\Entity\Partida')->findAll();
-	  $partidas = array_map(function($partida) {
-                return $this->convertToArray($partida); },
-                $partidas);
-    return $partidas;
-  }
+	/**
+	 * Constructor
+	 * @param Container $container
+	 */
+	public function __construct(Container $container) {
+		$this->logger = $container->get('logger');
+	}
 
-  /* Create partida
-  *
-  *  @param array|object|null $data
-  *  @return object
-  */
-  public function createPartida($data)
-  {
-    $partida = new Partida();
-    if ($data != null) {
-      $partida->setNombre($data['nombre']);
-      if ($this->checkPartida($partida) == null) {
-        if (empty($data['fecha']) || $data['fecha'] == null) {
-          $data['fecha'] = date('d/m/Y');
-        }
-        $partida->setFecha(DateTime::createFromFormat('d/m/Y', $data['fecha']));
-        if (empty($data['codigo']) || $data['codigo'] == null) {
-          $data['codigo'] = $this->generateRandomCodigo();
-        }
-        $partida->setCodigo($data['codigo']);
-        $partida->setEstado(1);
-        $this->getEntityManager()->persist($partida);
-        $this->getEntityManager()->flush();
-      }
-      $partida = $this->checkPartida($partida);
-    }
-    return $this->convertToArray($partida);
-  }
-  
-  /* Comprueba si la partida existe */
-  public function checkPartida($partida)
-  {
-    $partida = $this->getEntityManager()->getRepository('App\Entity\Partida')->find($partida->getNombre());
-    return $partida;
-  }
+	/**
+	 * Obtiene todas las partidas almacenadas
+	 */
+	public function getPartidas() {
+		//Obtenemos las partidas
+		$partidas = $this->getEntityManager()->getRepository('App\Entity\Partida')->findAll();
+		//Mapeamos el resultado
+		$partidas = array_map(function($partida) {
+			return $this->convertToArray($partida); },
+			$partidas);
+		//Devolvemos las partidas
+		return $partidas;
+	}
 
-  /* Genera el código inicial de la partida */
-  public function generateRandomCodigo($length = 6) {
-    $characters = 'ABCDEF';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-  }
+	/**
+	 *	Crea una partida nueva
+	 *  @param array|object|null $data
+	 *  @return object
+	 */
+	public function createPartida($data) {
+		$partida = new Partida();
+		if ($data != null) {
+			$partida->setNombre($data['nombre']);
+			//Comprobamos si ya existe una partida con ese nombre
+			if ($this->checkPartida($partida) == null) {
+				if (empty($data['fecha']) || $data['fecha'] == null) {
+					//Establecemos la fecha actual si no se ha indicado otra
+					$data['fecha'] = date('d/m/Y');
+				}
+				//Formateamos y guardamos la fecha en el objeto partida
+				$partida->setFecha(DateTime::createFromFormat('d/m/Y', $data['fecha']));
 
-  /**
-   * Actualiza el estado de la partida
-   */
-  public function updatePartida($data, $nombre)
-  {
-    $partida = new Partida();
-    $partida->setNombre($nombre);
-    $partida = $this->checkPartida($partida);
-    if ($partida != null) {
-      if ($data != null) {
-        $partida->setEstado($data['estado']);
-        $this->getEntityManager()->merge($partida);
-        $this->getEntityManager()->flush();
-      }
-    }
-    return $this->convertToArray($partida);
-  }
+				//Generamos un código aleatorio si no se ha indicado otro
+				if (empty($data['codigo']) || $data['codigo'] == null) {
+					$data['codigo'] = $this->generateRandomCodigo();
+				}
 
-  private function convertToArray(Partida $partida) {
-    return array(
-      'nombre' => $partida->getNombre(),
-      'fecha'  => $partida->getFecha(),
-      'codigo' => $partida->getCodigo(),
-      'estado' => $partida->getEstado()
-    );
-  }
+				$partida->setCodigo($data['codigo']);
+				$partida->setEstado(1);
+				//Guardamos la partida
+				$this->getEntityManager()->persist($partida);
+				$this->getEntityManager()->flush();
+			}
+			$partida = $this->checkPartida($partida);
+		}
+		return $this->convertToArray($partida);
+	}
+
+	/*
+	 * Comprueba si la partida ya existe 
+	 */
+	public function checkPartida($partida) {
+		$partida = $this->getEntityManager()->getRepository('App\Entity\Partida')->find($partida->getNombre());
+		return $partida;
+	}
+
+	/* 
+	 * Genera el código inicial de la partida 
+	 */
+	public function generateRandomCodigo($length = 6) {
+		$characters = 'ABCDEF';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
+
+	/**
+	 * Actualiza el estado de la partida
+	 */
+	public function updatePartida($data, $nombre) {
+		$partida = new Partida();
+		$partida->setNombre($nombre);
+		//Comprobamos si la partida a actualizar existe
+		$partida = $this->checkPartida($partida);
+		if ($partida != null) {
+			if ($data != null) {
+				$partida->setEstado($data['estado']);
+				//Actualizamos la partida
+				$this->getEntityManager()->merge($partida);
+				$this->getEntityManager()->flush();
+			}
+		}
+		return $this->convertToArray($partida);
+	}
+
+	/*
+	 * Mapeo de atributos de partidas
+	 */
+	private function convertToArray(Partida $partida) {
+		return array(
+		'nombre' => $partida->getNombre(),
+		'fecha'  => $partida->getFecha(),
+		'codigo' => $partida->getCodigo(),
+		'estado' => $partida->getEstado()
+		);
+	}
 }
